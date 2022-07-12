15d946e16d44112683e5c4a2c260cf91778e561d
Pradip Nichite <prnichite@gmail.com>
2022-07-12 21:28:26 +0530

    Added Spacy CLI

--- ml_app.py
+++ ml_app.py
@@ -5,7 +5,8 @@ import spacy
 import sklearn
 import string
 # python -m spacy download en_core_web_sm
-
+spacy.cli.download("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm-3.0.0\en_core_web_sm-3.0.0")
 nlp = spacy.load("en_core_web_sm")
 stop_words = nlp.Defaults.stop_words
 punctuations = string.punctuation
